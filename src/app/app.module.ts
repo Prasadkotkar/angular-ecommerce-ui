81e8bb74c4f9b236497ac66d746691b1824b7e55
Prasad <prasadkotkar.pk27@gmail.com>
2023-09-23 16:04:31 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.service';
+import { HttpClientModule } from '@angular/common/http';
+// import { Routes } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component' //*
+const routes: Routes =[  //order of the routes is important. first match wins
+    {path: 'product/:id', component: ProductDetailsComponent},
+    {path: 'search/:keyword', component: ProductListComponent},
+    {path: 'category/:id', component: ProductListComponent},
+    {path: 'category', component: ProductListComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: '', redirectTo: '/products', pathMatch: 'full'},
+    {path: '**', redirectTo: '/products', pathMatch: 'full'},  //this is a generic wildcard. it will match on anything that didn't match above routes
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),  //*
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule //*
+  ],
+  providers: [ProductService], //*
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  
+  // private baseUrl = 'http://localhost:8081/api/products?size=100';
+  private baseUrl = 'http://localhost:8081/api/products';
+
+  private categoryUrl = 'http://localhost:8081/api/product-category';
+  constructor(private httpClient: HttpClient) { };
+
+  getProduct(theProductId: number): Observable<Product> {
+
+    // need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+      // need to build URL base on category id +
+      const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+
+      return this.httpClient.get<GetResponseProduct>(searchUrl)
+             .pipe(map(response => response._embedded.products)
+             );
+  }
+
+  searchProducts(theKeyword: string) : Observable<Product[]> {
+      // need to build URL base on keyword
+      const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+      return this.httpClient.get<GetResponseProduct>(searchUrl)
+            .pipe(map(response => response._embedded.products)
+            );
+      
+      
+    }
+    
+  getProductCategories(): Observable<ProductCategory[]> {
+      return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl)
+            .pipe(map(response => response._embedded.productCategory)
+            );
+  }
+}
+
+interface GetResponseProduct {
+    _embedded: {
+      products: Product[];
+    }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
+}
